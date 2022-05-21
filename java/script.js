79830aa3dd4dd92d568d1561d91857cc6068a0b2
Kevin Knight <ksknight81@gmail.com>
2022-05-21 19:53:22 -0400

    updated javascript to change lowercase values for prompts

--- java/script.js
+++ java/script.js
@@ -3,26 +3,31 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-//Length of password
+//Length of password = var characterNumber
 
 //what character types?  loop and validate each, with atleast one requirement: lowercase, uppercase, numeric, and/or special characters
 var passwordQuestions = function() {
-var characterNumber = window.prompt('How many characters would you like your password to be');
-var lowerCase = window.prompt('Would you like to have a lowercase value?');
-var upperCase = window.prompt('Would you like to have an Uppercase value?');
-var numberValue = window.prompt('Would you like to add a number?');
-var specialCharacters = window.prompt('Would you like to use special characters?');
+var characterNumber = window.prompt('How many characters would you like your password to be?');
+var lowerCase = window.prompt('Would you like to have a lowercase value?  Enter Yes or No');
+var upperCase = window.prompt('Would you like to have an Uppercase value?  Enter Yes or No');
+var numberValue = window.prompt('Would you like to add a number?  Enter Yes or No');
+var specialCharacters = window.prompt('Would you like to use special characters?  Enter Yes or No');
 
 //validate proper values - where == yes or YES for each variable
-
+characterNumber = characterNumber.toLowerCase();
+lowerCase = lowerCase.toLowerCase();
+upperCase = upperCase.toLowerCase();
+numberValue = numberValue.toLowerCase();
+specialCharacters = specialCharacters.toLowerCase();
 
 //show values on console
-console.log(characterNumber)
-console.log(lowerCase)
-console.log(upperCase)
-console.log(numberValue)
-console.log(specialCharacters)
+console.log("how many characters? " + characterNumber)
+console.log("Do i need lowercase? " + lowerCase)
+console.log("Do i need Uppercase? " + upperCase)
+console.log("Do i need a number? " + numberValue)
+console.log("Do i need special characters? " + specialCharacters)
 }
+
 
 // Write password to the #password input
 function writePassword() {
@@ -33,10 +38,14 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//randomize values
+// if (numberValue === "yes")  {
+ // Math.floor(Math.random() * 10);
+ //console.log(numberValue)
+// }
 
-//display password in prompt
+// Add event listener to generate buton
+generateBtn.addEventListener("click", writePassword);
 
 // Variables
 
